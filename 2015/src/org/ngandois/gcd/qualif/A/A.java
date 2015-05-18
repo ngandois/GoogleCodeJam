251e4a31cbfd456f9fd506ca100a3f4fae38d685
ngandois <ngandois@users.noreply.github.com>
2015-05-18 10:41:35 +0200

    Update A.java
    Replace quickParse(...) by Character.getNumericValue(...)

--- 2015/src/org/ngandois/gcd/qualif/A/A.java
+++ 2015/src/org/ngandois/gcd/qualif/A/A.java
@@ -28,7 +28,7 @@ public class A implements Function<Exercise.TestCase, Exercise.TestResult>{
         char[] people =  c.data.substring(c.data.indexOf(" ") + 1, c.data.length()).toCharArray();
 
         for (int shynessCursor = 0; shynessCursor < people.length && nbStandUp < maxShyness; shynessCursor++) {
-            int nextNbStandUp = quickParse(people[shynessCursor]);
+            int nextNbStandUp = Character.getNumericValue(people[shynessCursor]);
 
             if (nbStandUp + nbToInvite < shynessCursor){
                 nbToInvite+= shynessCursor - (nbStandUp + nbToInvite);
@@ -38,23 +38,5 @@ public class A implements Function<Exercise.TestCase, Exercise.TestResult>{
         }
 
         return new Exercise.TestResult(c.testNumber, new String[]{String.valueOf(nbToInvite)});
-    }
-
-
-    private static int quickParse(char c){
-        switch (c){
-            case '0': return 0;
-            case '1': return 1;
-            case '2': return 2;
-            case '3': return 3;
-            case '4': return 4;
-            case '5': return 5;
-            case '6': return 6;
-            case '7': return 7;
-            case '8': return 8;
-            case '9': return 9;
-        }
-        return -1;
-
     }
 }
