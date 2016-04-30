8ec069919dc5b8b6e70ff3f2f1594b480c4aa04d
ngandois <ngandois@users.noreply.github.com>
2016-04-30 14:56:41 +0200

    2016 round1 A (The Last Word)

---
+++ gcj/src/main/java/org/ngandois/gcd/MMXVI/round/one/A/A.java
@@ -0,0 +1,41 @@
+package org.ngandois.gcd.MMXVI.round.one.A;
+
+import org.ngandois.gcd.tools.*;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+
+public class A extends CaseResolver {
+
+    public static void main(String[] args) throws IOException {
+        A solver = new A();
+        new ExerciseResolver("A-large-practice", solver).resolve();
+    }
+
+    public A(){
+        super(new LineByLineCaseReader(new InputSpaceParsing()));
+    }
+
+
+    @Override
+    public Exercise.TestResult apply(Exercise.TestCase c) {
+        char[] chars = c.data.get(0).get(0).toCharArray();
+
+        StringBuilder buf = new StringBuilder(chars.length);
+        buf.append(chars[0]);
+
+        for (int i = 1; i < chars.length; i++) {
+            if(chars[i] >= buf.charAt(0))
+                buf.insert(0, chars[i]);
+            else
+                buf.append(chars[i]);
+        }
+
+        return new Exercise.TestResult(c.testNumber, buf.toString());
+    }
+}
