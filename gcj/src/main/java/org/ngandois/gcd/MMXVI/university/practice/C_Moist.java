117a3e7ac0dba7ab21ed4af13a0e3d6d216ac77d
ngandois <ngandois@users.noreply.github.com>
2017-02-26 18:05:21 +0100

    Resolved Moist (Practice Round APAC test 2016)

---
+++ gcj/src/main/java/org/ngandois/gcd/MMXVI/university/practice/C_Moist.java
@@ -0,0 +1,45 @@
+package org.ngandois.gcd.MMXVI.university.practice;
+
+import org.ngandois.gcd.tools.CaseResolver;
+import org.ngandois.gcd.tools.ExerciseResolver;
+import org.ngandois.gcd.tools.InputNoParsing;
+import org.ngandois.gcd.tools.ReadNbLineFirstCaseReader;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class C_Moist extends CaseResolver {
+
+  public static void main(String[] args) throws IOException {
+    C_Moist solver = new C_Moist();
+    new ExerciseResolver("C-small-practice-1", solver).resolve();
+    new ExerciseResolver("C-small-practice-2", solver).resolve();
+  }
+
+  public C_Moist() {
+    super(new ReadNbLineFirstCaseReader(new InputNoParsing()));
+  }
+
+  @Override
+  public String solve(List<List<String>> data) {
+    List<String> cards = data.stream().map(l -> l.get(0)).collect(Collectors.toList());
+
+
+    int cost = 0;
+    String higherName = cards.get(0);
+    for (int i = 1; i < cards.size(); i++) {
+      if (higherName.compareTo(cards.get(i)) < 0)
+        higherName = cards.get(i);
+      else
+        cost++;
+
+    }
+
+
+    return String.valueOf(cost);
+  }
+
+
+}
